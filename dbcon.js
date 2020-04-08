4ed837a42556a602351db9bc59c796a150184da2
macijona <55888902+macijona@users.noreply.github.com>
2020-04-08 07:29:30 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu', 
-  user            : 'cs340_onid',
-  password        : 'password',
-  database        : 'cs340_onid'
+  user            : 'cs340_macijona',
+  password        : '9EDF2lmUxZzIoazK',
+  database        : 'cs340_macijona'
 });
 
 module.exports.pool = pool;
